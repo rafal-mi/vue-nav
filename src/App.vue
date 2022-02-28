69468ad4ce0911ef53c8848356763a7d4c131639
Rafal Mi <rafal.m4@gmail.com>
2022-02-28 22:37:25 +0100

    Initial project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import TopNav from "@/components/TopNav.vue";
+
+</script>
+
+<template>
+  <TopNav />
+  <header>
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+/* @import "@/assets/app.css"; */
+
+</style>
---
+++ src/components/TopNav.vue
@@ -0,0 +1,44 @@
+<script setup>
+// import { RouterLink, RouterView } from "vue-router";
+</script>
+
+
+/* eslint-disable prettier/prettier */
+<template>
+  <div class="topnav">
+    <router-link to="/"><a class="active" href="#home">Home</a></router-link>
+    <a href="#news">News</a>
+    <a href="#contact">Contact</a>
+    <RouterLink to="/about"><a href="#about">About</a></RouterLink>
+  </div>
+</template>
+
+<style scoped>
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+  background-color: #04aa6d;
+  color: white;
+}
+</style>
